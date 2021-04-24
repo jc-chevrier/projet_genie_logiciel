2f76b058b195d127210c0543026664a561c1cae1
yousra-hm <hadjmessaoudyousra99@gmail.com>
2021-04-24 17:26:30 +0200

    dev (sql): insertion rôles
    Ajout du script d'insertion des rôles dans la table rôle
    
    ---

--- src/main/sql/fr.ul.miage.m1.projet_genie_logiciel/initialisation/ajout-roles.sql
+++
@@ -1,9 +0,0 @@
-insert into ROLE (ID , LIBELLE) values (1 , 'Directeur'),
-insert into ROLE (ID , LIBELLE) values (2 , 'Maitre hotel');
-insert into ROLE (ID , LIBELLE) values (3 , 'Cuisinier');
-insert into ROLE (ID , LIBELLE) values (4 , 'Serveur');
-insert into ROLE (ID , LIBELLE) values (5 , 'Assistant de service')
-
-
-
- 
