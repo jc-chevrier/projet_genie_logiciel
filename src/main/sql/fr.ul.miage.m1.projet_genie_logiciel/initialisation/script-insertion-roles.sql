9f58b7e34ad88bcaab017f2923f658b1079a2a87
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2021-04-25 01:05:41 +0200

    dev (sql): amélioration scripts
    ajouts des 'alter sequence ...'
    
    -

--- src/main/sql/fr.ul.miage.m1.projet_genie_logiciel/initialisation/script-insertion-roles.sql
+++ src/main/sql/fr.ul.miage.m1.projet_genie_logiciel/initialisation/script-insertion-roles.sql
@@ -4,6 +4,6 @@ INSERT INTO ROLE (ID, LIBELLE) values (3, 'Cuisinier');
 INSERT INTO ROLE (ID, LIBELLE) values (4, 'Serveur');
 INSERT INTO ROLE (ID, LIBELLE) values (5, 'Assistant service');
 
-
+ALTER SEQUENCE role_id_seq RESTART WITH 6;
 
  
