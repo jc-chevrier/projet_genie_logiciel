ab8d7d4a1e4e0263182664d53b41e0f288c1defd
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2021-05-01 01:16:26 +0200

    feature (test) ajout du test des cas de suppresion d'unité
    -
    
    user story #57

--- src/test/java/fr/ul/miage/m1/projet_genie_logiciel/UniteTest.java
+++ src/test/java/fr/ul/miage/m1/projet_genie_logiciel/UniteTest.java
@@ -11,6 +11,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Unite")
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class UniteTest {
     private static ORM orm;
     private static UI ui;
@@ -25,6 +26,7 @@ public class UniteTest {
     }
 
     @Test
+    @Order(1)
     @DisplayName("Test - ajouter une unité - cas 1 : n-uplet unité bien ajoutée")
     void testAjouterUniteCasBienAjoute() {
         //On simule les saisies de l'ajout dans ce fichier.
@@ -42,6 +44,7 @@ public class UniteTest {
     }
 
     @Test
+    @Order(2)
     @DisplayName("Test - ajouter une unité - cas 2 : n-uplet unité bien ajoutée avec bon libellé")
     void testAjouterUniteCas2BonLibelle() {
         //On simule les saisies de l'ajout dans ce fichier.
@@ -56,5 +59,44 @@ public class UniteTest {
                                                                     "OFFSET 0 " +
                                                                     "LIMIT 1", Unite.class);
         assertEquals("libellé test ajouter cas 2 bon libellé", uniteInseree.getLibelle());
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Test - supprimer une unité - cas 1 : n-uplet unité bien supprimée")
+    void testSupprimerUniteCas1Supprimee() {
+        Unite unite3 = new Unite();
+        unite3.setLibelle("libellé unité 3");
+        orm.persisterNUplet(unite3);
+
+        //On simule les saisies de la suppression dans ce fichier.
+        System.setIn(UniteTest.class.getResourceAsStream("./saisies/unite_test/supprimer_cas_1.txt"));
+        ui.reinitialiserScanner();
+
+        //Unité existant avant.
+        Unite uniteAvant = (Unite) orm.chercherNUpletAvecPredicat("WHERE ID = 3", Unite.class);
+        assertNotNull(uniteAvant);
+
+        //On simule le scénario de suppression.
+        UniteControleur.supprimer();
+
+        //Unité suppirmée après.
+        Unite uniteApres = (Unite) orm.chercherNUpletAvecPredicat("WHERE ID = 3", Unite.class);
+        assertNull(uniteApres);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test - supprimer une unité - cas 2 : aucun n-uplet unité trouvé")
+    void testSupprimerUniteCas2Supprimee() {
+        //On vide la table unité.
+        orm.chercherTousLesNUplets(Unite.class).forEach(orm::supprimerNUplet);
+
+        //On simule les saisies de la suppression dans ce fichier.
+        System.setIn(UniteTest.class.getResourceAsStream("./saisies/unite_test/supprimer_cas_2.txt"));
+        ui.reinitialiserScanner();
+
+        //On simule le scénario de suppression.
+        UniteControleur.supprimer();
     }
 }
