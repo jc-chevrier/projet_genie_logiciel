9b33be59d065529cd7f0c9bf3bbcd4930a123386
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2021-05-01 17:33:17 +0200

    test (sql) évolution des scripts sql et création de .bat pour reconstituer les bases de données de dev et de test
    -
    
    user story #68

---
+++ src/test/sql/fr.ul.miage.m1.projet_genie_logiciel/reconstruire_bdd_test.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+
+SET configurationFile=..\..\resources\fr\ul\miage\m1\projet_genie_logiciel\configuration\configuration_bdd_test.properties
+FOR /F "tokens=1,2 delims==" %%G IN (%configurationFile%) DO (set %%G=%%H)
+
+SET PGPASSWORD=%motDePasse%
+SET scriptsChemin=..\..\..\main\sql\fr.ul.miage.m1.projet_genie_logiciel\
+psql -h %hote% -p %port% -U %utilisateur% -d postgres -q -v baseDeDonnees=%baseDeDonnees% < %scriptsChemin%initialisation\script_creation_bdd.sql
+psql -h %hote% -p %port% -U %utilisateur% -d %baseDeDonnees% -q < %scriptsChemin%initialisation\script_creation_tables.sql
+psql -h %hote% -p %port% -U %utilisateur% -d %baseDeDonnees% -q < %scriptsChemin%initialisation\script_insertion_roles.sql
+psql -h %hote% -p %port% -U %utilisateur% -d %baseDeDonnees% -q < %scriptsChemin%jeu_de_donnees\script_insertion_comptes.sql
