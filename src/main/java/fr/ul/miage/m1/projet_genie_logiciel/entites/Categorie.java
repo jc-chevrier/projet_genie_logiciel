24b7dbe95893dfac5781a0ca8b3293bdd522d7c9
yousra-hm <hadjmessaoudyousra99@gmail.com>
2021-05-02 00:37:01 +0200

    feature(Entites): Ajout de l'entité catégorie
    --
    
    --

---
+++ src/main/java/fr/ul/miage/m1/projet_genie_logiciel/entites/Categorie.java
@@ -0,0 +1,41 @@
+package fr.ul.miage.m1.projet_genie_logiciel.entites;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Entité catégorie.
+ *
+ * @author CHEVRIER, HADJ MESSAOUD, LOUGADI
+ */
+public class Categorie extends Entite {
+
+    //Nom de la table correspondant à l'entité.
+    public static String NOM_TABLE = "CATEGORIE";
+    //Structure de l'entité [attribut -> type].
+    public static Map<String, Class> STRUCTURE;
+
+    //Initialisation de la structure.
+    static {
+        STRUCTURE = new HashMap<String, Class>();
+        STRUCTURE.put("ID", Integer.class);
+        STRUCTURE.put("LIBELLE", String.class);
+    }
+
+    public Categorie() {
+        super();
+    }
+
+    public Categorie(@NotNull Map<String, Object> attributs) {
+        super(attributs);
+    }
+
+    public String getLibelle() {
+        return (String) get("LIBELLE");
+    }
+
+    public void setLibelle(@NotNull String libelle) {
+        set("LIBELLE", libelle);
+    }
+}
