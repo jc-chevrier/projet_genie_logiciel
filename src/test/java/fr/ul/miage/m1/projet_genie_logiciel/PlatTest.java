32bf812326849f1bb10cb92145df5df49a71eb2e
yousra-hm <hadjmessaoudyousra99@gmail.com>
2021-05-11 20:46:31 +0200

    feature (PlatTest): ajout des tests de la méthode supprimer
    --
    
    --

--- src/test/java/fr/ul/miage/m1/projet_genie_logiciel/PlatTest.java
+++ src/test/java/fr/ul/miage/m1/projet_genie_logiciel/PlatTest.java
@@ -1,12 +1,10 @@
 package fr.ul.miage.m1.projet_genie_logiciel;
 
 import fr.ul.miage.m1.projet_genie_logiciel.controleurs.PlatControleur;
-import fr.ul.miage.m1.projet_genie_logiciel.entites.Categorie;
-import fr.ul.miage.m1.projet_genie_logiciel.entites.Compte;
-import fr.ul.miage.m1.projet_genie_logiciel.entites.Plat;
+import fr.ul.miage.m1.projet_genie_logiciel.controleurs.UniteControleur;
+import fr.ul.miage.m1.projet_genie_logiciel.entites.*;
 import fr.ul.miage.m1.projet_genie_logiciel.ui.UI;
 import org.junit.jupiter.api.*;
-import fr.ul.miage.m1.projet_genie_logiciel.entites.Entite;
 import fr.ul.miage.m1.projet_genie_logiciel.orm.ORM;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
@@ -64,5 +62,81 @@ public class PlatTest {
 
         //On simule le scénario de listing.
         PlatControleur.lister();
+    }
+    @Test
+    @Order(3)
+    @DisplayName("Test : supprimer un plat - cas 1 : plat bien supprimé")
+    void testSupprimerCas1BienSupprimee() {
+        //On ajoute une catégorie pour pouvoir ajouter un plat.
+        Categorie categorie = new Categorie();
+        categorie.setLibelle("libellé1");
+        orm.persisterNUplet(categorie);
+        //On ajoute un plat à lister.
+        Plat plat = new Plat();
+        plat.setLibelle("libellé");
+        plat.setCarte(0);
+        plat.setPrix(2.5);
+        plat.setIdCategorie(1);
+        orm.persisterNUplet(plat);
+
+        //On simule les saisies de la suppression dans ce fichier.
+        System.setIn(PlatTest.class.getResourceAsStream("./saisies/plat_test/supprimer_cas_1.txt"));
+        ui.reinitialiserScanner();
+
+        int nbPlatsAvant = orm.compterTousLesNUplets(Plat.class);
+
+        //On simule le scénario de suppression.
+       PlatControleur.supprimer();
+
+        //Un plat a du être supprimé.
+        int nbPlatsApres = orm.compterTousLesNUplets(Plat.class);
+        assertEquals(nbPlatsAvant - 1, nbPlatsApres);
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Test : supprimer un plat - cas 2 : plat supprimé correct")
+    void testSupprimerCas2Correcte() {
+        //On ajoute une catégorie pour pouvoir ajouter un plat.
+        Categorie categorie = new Categorie();
+        categorie.setLibelle("libellé1");
+        orm.persisterNUplet(categorie);
+        //On ajoute un plat à lister.
+        Plat plat = new Plat();
+        plat.setLibelle("libellé");
+        plat.setCarte(0);
+        plat.setPrix(2.5);
+        plat.setIdCategorie(1);
+        orm.persisterNUplet(plat);
+
+        //On simule les saisies de la suppression dans ce fichier.
+        System.setIn(PlatTest.class.getResourceAsStream("./saisies/plat_test/supprimer_cas_2.txt"));
+        ui.reinitialiserScanner();
+
+        //Plat existant avant.
+        Plat platAvant = (Plat) orm.chercherNUpletAvecPredicat("WHERE ID = 3", Plat.class);
+        assertNotNull(platAvant);
+
+        //On simule le scénario de suppression.
+        PlatControleur.supprimer();
+
+        //Plat suppirmé après.
+        Plat platApres = (Plat) orm.chercherNUpletAvecPredicat("WHERE ID = 3", Plat.class);
+        assertNull(platApres);
+    }
+
+    @Test
+    @Order(5)
+    @DisplayName("Test : supprimer un plat - cas 3 : aucun plat trouvé")
+    void testSupprimerCas3PasTrouvees() {
+        //On vide la table plat.
+        orm.chercherTousLesNUplets(Plat.class).forEach(orm::supprimerNUplet);
+
+        //On simule les saisies de la suppression dans ce fichier.
+        System.setIn(PlatTest.class.getResourceAsStream("./saisies/plat_test/supprimer_cas_3.txt"));
+        ui.reinitialiserScanner();
+
+        //On simule le scénario de suppression.
+        PlatControleur.supprimer();
     }
 }
