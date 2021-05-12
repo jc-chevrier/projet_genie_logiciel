b81c0624e226f34435aa33a32af434ad8d015fed
yousra-hm <hadjmessaoudyousra99@gmail.com>
2021-05-13 00:18:12 +0200

    feature (PlatControleur): ajout de la méthode lister les plats disponible de la carte du jour
    ajouter la méthode qui permet de lister uniquement les plats disponibles de la carte du jour
    
    user story #69

--- src/main/java/fr/ul/miage/m1/projet_genie_logiciel/controleurs/Fonctionnalite.java
+++ src/main/java/fr/ul/miage/m1/projet_genie_logiciel/controleurs/Fonctionnalite.java
@@ -61,7 +61,7 @@ public class Fonctionnalite {
 
         List<Fonctionnalite> fonctionnalitesServeur = new ArrayList<Fonctionnalite>(
                 Arrays.asList(new Fonctionnalite("Lister tous les plats de la carte", PlatControleur::listerCarte),
-                              new Fonctionnalite("Lister tous les plats disponibles de la carte", AccueilControleur::consulter), //TODO à modifier
+                              new Fonctionnalite("Lister tous les plats disponibles de la carte", PlatControleur::ListerDisponibleCarte),
                               new Fonctionnalite("Lister les plats disponibles de la carte pour une catégorie", AccueilControleur::consulter), //TODO à modifier
                               new Fonctionnalite("Lister les catégories de plats disponibles de la carte", AccueilControleur::consulter), //TODO à modifier
                               new Fonctionnalite("Ajouter une commande", AccueilControleur::consulter),//TODO à modifier
