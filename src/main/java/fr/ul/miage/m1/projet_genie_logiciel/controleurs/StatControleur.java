0d4b801966ff38d538815f5267b5fc2e37e7c3a7
jc-chevrier <47176536+jc-chevrier@users.noreply.github.com>
2021-05-25 23:41:37 +0200

    dev (ORM, StatControleur): ajout d'une méthode pour une recherche libre dans ORM
    -
    
    -

---
+++ src/main/java/fr/ul/miage/m1/projet_genie_logiciel/controleurs/StatControleur.java
@@ -0,0 +1,15 @@
+package fr.ul.miage.m1.projet_genie_logiciel.controleurs;
+
+/**
+ * Controleur pour les statistiques.
+ *
+ * @author CHEVRIER, HADJ MESSAOUD, LOUGADI
+ */
+public class StatControleur {
+    /**
+     * Constulter les statistiques générales.
+     */
+    public static void consulterGenerales() {
+
+    }
+}
